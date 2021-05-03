71eed8975ca34eb6c9a36cf0ee6eeee4a7bce6ed
kostiantyn.bilyznyi <kostya.bilyznyi@beetroot.se>
2021-05-03 18:26:05 +0300

    DEBUGIND ThemeHEADER

--- wp-content/themes/organic/inc/classes/OrganicHeader.class.php
+++ wp-content/themes/organic/inc/classes/OrganicHeader.class.php
@@ -3,20 +3,9 @@ class OrganicHeader {
 
 	function get_sale_section() {
 
-		$block = <<<HTML
-<div class="bg-bay-leaf py-2">
-	<div class="continer">
-		<a class="text-center" href="{$sale_bunner_url}">
-			<p class="font-size-14 text-white mb-0">
-				LOREM IPSUM
-				<span>
-					TEST
-				</span>
-			</p>
-		</a>
-	</div>
-</div>
-HTML;	
+		$block = <<<SALE
+<span>TEST</span>
+SALE;
 
 		return $block;
 	}
