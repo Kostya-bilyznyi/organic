6988cb240d6174b14a75cc7d2ebea1f8a5eaf0c1
kostiantyn.bilyznyi <kostya.bilyznyi@beetroot.se>
2021-05-03 19:36:18 +0300

    DEBUGIND ThemeHEADER 12

--- wp-content/themes/organic/inc/classes/OrganicHeader.class.php
+++ wp-content/themes/organic/inc/classes/OrganicHeader.class.php
@@ -1,49 +1,14 @@
 <?php
 class OrganicHeader {
-	public $acf_fields;
-
-	function __construct() {
-		$this->acf_fields = get_field('header', 'options');
-	}
-
-	/**
-	 * Sale section
-	 */
-/*	function get_sale_section() {
-		$sale_bunner_link = $this->acf_fields['sale_bunner_link'];
-		$sale_bunner_tiile = $this->acf_fields['sale_bunner_link']['title'];
-		$sale_bunner_url = $this->acf_fields['sale_bunner_link']['url'];
-		$sale_bunner_icon = $this->acf_fields['sale_bunner_icon'];
-
-		$block = <<<HTML
-<div class="bg-bay-leaf py-2">
-	<div class="continer">
-		<a class="text-center" href="{$sale_bunner_url}">
-			<p class="font-size-14 text-white mb-0">
-				{$sale_bunner_tiile}
-				<span>
-					{$sale_bunner_icon}
-				</span>
-			</p>
-		</a>
-	</div>
-</div>
-HTML;
-
-		return $block;
-	} */
 
 	function get_header_logo() {
-		$home_url = home_url('/');
-		$logo_img_url = $this->acf_fields['logo'];
-
-		$block = <<<LOGO
+		$block = <<<HTML
 <div class="col-lg-2">
-	<a class="navbar-brand mx-auto d-none d-lg-block" href="{$home_url}">
-		<img class="navbar-brand__img" src="{$logo_img_url}" alt="logo"/>
+	<a class="navbar-brand mx-auto d-none d-lg-block" href="#">
+		TEST LINK
 	</a>
 </div>
-LOGO;	
+HTML;
 
 		return $block;
 	}
