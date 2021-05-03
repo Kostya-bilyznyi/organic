2b30860bbe8b154ece97550951694897683aa526
kostiantyn.bilyznyi <kostya.bilyznyi@beetroot.se>
2021-05-03 18:47:42 +0300

    DEBUGIND ThemeHEADER 8

--- wp-content/themes/organic/inc/classes/OrganicHeader.class.php
+++ wp-content/themes/organic/inc/classes/OrganicHeader.class.php
@@ -1,30 +1,9 @@
 <?php
 class OrganicHeader {
-	public $acf_fields;
-
-	function __construct() {
-		$this->acf_fields = get_field('header', 'options'); 
-	}
-
 	function get_sale_section() {
-		$sale_bunner_link = $this->acf_fields['sale_bunner_link'];
-		$sale_bunner_tiile = $sale_bunner_link['title'];
-		$sale_bunner_url = $sale_bunner_link['url'];
-		$sale_bunner_icon = $this->acf_fields['sale_bunner_icon'];
 
 		$block = <<<HTML
-<div class="bg-bay-leaf">
-	<div class="continer">
-		<a class="text-center" href="{$sale_bunner_url}">
-			<p class="font-size-14 text-white mb-0">
-				{$sale_bunner_tiile}
-				<span>
-					{$sale_bunner_icon}
-				</span>
-			</p>
-		</a>
-	</div>
-</div>
+<span>test</span>
 HTML;	
 
 		return $block;
